e124d35440ce683ba685fa658f46991001b18f65
JL <5397809+b4lisong@users.noreply.github.com>
2024-11-18 07:18:04 -0800

    TODO/info: 24.11 for Darwin has an issue with nixpkgs; upgrade when resolved

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
     impermanence.url = "github:nix-community/impermanence";
 
     # for macos
+    # TODO: update to 24.11; watch https://github.com/NixOS/nixpkgs/pull/356960
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
     nix-darwin = {
       url = "github:lnl7/nix-darwin";
