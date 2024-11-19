59ac8d80083717d76cf7534d10d1e407f4f340f8
JL <5397809+b4lisong@users.noreply.github.com>
2024-11-18 17:17:54 -0800

    feat: define macOS customization

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,14 +1,16 @@
-{ pkgs, ... }:
-
-  ###################################################################################
-  #
-  #  macOS's System configuration
-  #
-  #  All the configuration options are documented here:
-  #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
-  #
-  ###################################################################################
+{pkgs, ...}:
+###################################################################################
+#
+#  macOS's System configuration
+#
+#  All the configuration options are documented here:
+#    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+#
+###################################################################################
 {
+  # Add ability to use TouchID for sudo
+  security.pam.enableSudoTouchIdAuth = true;
+
   system = {
     stateVersion = 5;
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
@@ -19,18 +21,184 @@
     '';
 
     defaults = {
-      menuExtraClock.Show24Hour = true;  # show 24 hour clock
+      menuExtraClock.Show24Hour = true; # show 24 hour clock
 
-      # other macOS's defaults configuration.
-      # ......
+      # customize dock
+      dock = {
+        autohide = true; # automatically hide and show the dock
+        show-recents = false; # do not show recent apps in dock
+        # do not automatically rearrange spaces based on most recent use.
+        mru-spaces = false;
+        expose-group-by-app = true; # Group windows by application
+
+        # customize Hot Corners
+        wvous-tl-corner = 2; # top-left - Mission Control
+        wvous-tr-corner = 4; # top-right - Desktop
+        wvous-bl-corner = 3; # bottom-left - Application Windows
+        wvous-br-corner = 13; # bottom-right - Lock Screen
+      };
+
+      # customize finder
+      finder = {
+        _FXShowPosixPathInTitle = true; # show full path in finder title
+        AppleShowAllExtensions = true; # show all file extensions
+        FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+        QuitMenuItem = true; # enable quit menu item
+        ShowPathbar = true; # show path bar
+        ShowStatusBar = true; # show status bar
+      };
+
+      # customize trackpad
+      trackpad = {
+        Clicking = true; # enable tap to click
+        TrackpadRightClick = true; # enable two finger right click
+        TrackpadThreeFingerDrag = true; # enable three finger drag
+      };
+
+      # customize macOS
+      NSGlobalDomain = {
+        # `defaults read NSGlobalDomain "xxx"`
+        "com.apple.swipescrolldirection" = true; # enable natural scrolling(default to true)
+        "com.apple.sound.beep.feedback" = 0; # disable beep sound when pressing volume up/down key
+
+        # Appearance
+        AppleInterfaceStyle = "Dark"; # dark mode
+
+        AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
+        ApplePressAndHoldEnabled = true; # enable press and hold
+
+        # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+        # This is very useful for vim users, they use `hjkl` to move cursor.
+        # sets how long it takes before it starts repeating.
+        InitialKeyRepeat = 15; # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        # sets how fast it repeats once it starts.
+        KeyRepeat = 3; # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+        NSAutomaticCapitalizationEnabled = false; # disable auto capitalization
+        NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution
+        NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution
+        NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution
+        NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction
+        NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default
+        NSNavPanelExpandedStateForSaveMode2 = true;
+      };
+
+      # customize settings that not supported by nix-darwin directly
+      # Incomplete list of macOS `defaults` commands :
+      #   https://github.com/yannbertrand/macos-defaults
+      CustomUserPreferences = {
+        ".GlobalPreferences" = {
+          # automatically switch to a new space when switching to the application
+          AppleSpacesSwitchOnActivate = true;
+        };
+        NSGlobalDomain = {
+          # Add a context menu item for showing the Web Inspector in web views
+          WebKitDeveloperExtras = true;
+        };
+        "com.apple.finder" = {
+          AppleShowAllFiles = true;
+          ShowExternalHardDrivesOnDesktop = true;
+          ShowHardDrivesOnDesktop = true;
+          ShowMountedServersOnDesktop = true;
+          ShowRemovableMediaOnDesktop = true;
+          _FXSortFoldersFirst = true;
+          # When performing a search, search the current folder by default
+          FXDefaultSearchScope = "SCcf";
+        };
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.spaces" = {
+          # Display have separate spaces
+          #   true => disable this feature
+          #   false => enable this feature
+          "spans-displays" = true;
+        };
+        "com.apple.WindowManager" = {
+          EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
+          StandardHideDesktopIcons = 0; # Show items on desktop
+          HideDesktop = 0; # Do not hide items on desktop & stage manager
+          StageManagerHideWidgets = 0;
+          StandardHideWidgets = 0;
+        };
+        "com.apple.screensaver" = {
+          # Require password immediately after sleep or screen saver begins
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+        "com.apple.screencapture" = {
+          location = "~/Desktop";
+          type = "png";
+        };
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+        # Prevent Photos from opening automatically when devices are plugged in
+        "com.apple.ImageCapture".disableHotPlug = true;
+      };
+
+      loginwindow = {
+        GuestEnabled = false; # disable guest user
+        SHOWFULLNAME = true; # show full name in login window
+      };
+    };
+
+    # keyboard settings is not very useful on macOS
+    # the most important thing is to remap option key to alt key globally,
+    # but it's not supported by macOS yet.
+    keyboard = {
+      enableKeyMapping = true; # enable key mapping so that we can use `option` as `control`
+
+      # NOTE: do NOT support remap capslock to both control and escape at the same time
+      remapCapsLockToControl = false; # remap caps lock to control, useful for emac users
+      remapCapsLockToEscape = true; # remap caps lock to escape, useful for vim users
+
+      # swap left command and left alt
+      # so it matches common keyboard layout: `ctrl | command | alt`
+      #
+      # disabled, caused only problems!
+      swapLeftCommandAndLeftAlt = false;
+
+      userKeyMapping = [
+        # remap escape to caps lock
+        # so we swap caps lock and escape, then we can use caps lock as escape
+        {
+          HIDKeyboardModifierMappingSrc = 30064771113;
+          HIDKeyboardModifierMappingDst = 30064771129;
+        }
+      ];
     };
   };
-
-  # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
   programs.zsh.enable = true;
 
+  # Fonts
+  fonts = {
+    packages = with pkgs; [
+      # packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
+
+      source-sans
+      source-serif
+
+      # nerdfonts
+      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
+      (nerdfonts.override {
+        fonts = [
+          # symbols icon only
+          "NerdFontsSymbolsOnly"
+          # Characters
+          "SourceCodePro"
+        ];
+      })
+      julia-mono
+      dejavu_fonts
+    ];
+  };
 }
