14414e13298bea78653dd1f97f3f427e515d8b0f
JL <5397809+b4lisong@users.noreply.github.com>
2024-11-18 17:36:50 -0800

    feat/fix: remove esc/caps lock swap for now

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -151,9 +151,10 @@
     keyboard = {
       enableKeyMapping = true; # enable key mapping so that we can use `option` as `control`
 
+      # TODO: possible to disable when specific keyboard connected?
       # NOTE: do NOT support remap capslock to both control and escape at the same time
-      remapCapsLockToControl = false; # remap caps lock to control, useful for emac users
-      remapCapsLockToEscape = true; # remap caps lock to escape, useful for vim users
+      #remapCapsLockToControl = false; # remap caps lock to control, useful for emac users
+      #remapCapsLockToEscape = true; # remap caps lock to escape, useful for vim users
 
       # swap left command and left alt
       # so it matches common keyboard layout: `ctrl | command | alt`
@@ -161,14 +162,14 @@
       # disabled, caused only problems!
       swapLeftCommandAndLeftAlt = false;
 
-      userKeyMapping = [
-        # remap escape to caps lock
-        # so we swap caps lock and escape, then we can use caps lock as escape
-        {
-          HIDKeyboardModifierMappingSrc = 30064771113;
-          HIDKeyboardModifierMappingDst = 30064771129;
-        }
-      ];
+      #userKeyMapping = [
+      #  # remap escape to caps lock
+      #  # so we swap caps lock and escape, then we can use caps lock as escape
+      #  {
+      #    HIDKeyboardModifierMappingSrc = 30064771113;
+      #    HIDKeyboardModifierMappingDst = 30064771129;
+      #  }
+      #];
     };
   };
 
