af74c3a35c1cf56af450c5099b86d8b1790e993a
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-14 12:55:33 -0700

    feat: Finder customization

--- flake.nix
+++ flake.nix
@@ -135,6 +135,18 @@
                   "/Applications/Cursor.app"
                 ];
             };
+
+            # Customize Finder
+            finder = {
+                _FXShowPosixPathInTitle = true; # show full path in finder title
+                AppleShowAllExtensions = true; # show all file extensions
+                FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+                QuitMenuItem = true; # enable quit menu item
+                ShowPathbar = true; # show path bar
+                ShowStatusBar = true; # show status bar
+                FXDefaultSearchScope = "SCcf";      # Search current folder by default
+                FXPreferredViewStyle = "Nlsv";      # Default view: "icnv"=icon, "Nlsv"=list, "clmv"=column, "Flwv"=gallery
+            };
         };
       };
 
