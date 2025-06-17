55960808b40a76701aa3d5e4bdafadf28cf771ba
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-17 10:45:58 -0700

    chore: documentation for scanPaths

--- lib/modules.nix
+++ lib/modules.nix
@@ -1,10 +1,20 @@
+# This file extends the nixpkgs standard library and contains module helper functions.
+#
+# scanPaths scans a directory path and returns a list of paths to:
+#   - all subdirectories
+#   - all `.nix` files (except `default.nix`)
+# This makes it easier to add new modules without manually updating import lists.
+# Copied from https://raw.githubusercontent.com/ryan4yin/nix-config/refs/heads/main/lib/default.nix
 { lib, ... }:
 {
+  # scanPaths: Recursively finds all subdirectories and .nix files (except default.nix) in a given path.
   scanPaths = path:
     builtins.map
       (f: (path + "/${f}"))
+      # Get all attribute names (files/dirs) in the directory that are either:
       (builtins.attrNames (
         lib.attrsets.filterAttrs
+          # Keep if it's a directory, or a .nix file that's not default.nix
           (path: _type:
             (_type == "directory") ||
             (path != "default.nix" && lib.strings.hasSuffix ".nix" path))
