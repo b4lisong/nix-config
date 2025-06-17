72ab44c391b7f442f305464d18e6567132a7fe56
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-17 11:42:39 -0700

    chore: darwin system level config doc

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,14 @@
+/*
+System-level configuration common to all Darwin/macOS hosts
+Extends base system configuration in `modules/base.nix`
+*/
 { pkgs, ... }:
 {
+  # Enable Touch ID authentication for sudo
+  # This allows using fingerprint authentication when running sudo commands
   security.pam.services.sudo_local.touchIdAuth = true;
 
+  # System state version for compatibility
+  # This should be incremented when making breaking changes to the system configuration
   system.stateVersion = 6;
 }
