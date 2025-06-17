6e3445e2fb5481dce36c17e07b0360c1003892b0
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-17 12:42:46 -0700

    feat: zsh zoxide cd aliases

--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -15,6 +15,12 @@
       gs = "git status";
       ga = "git add";
       gc = "git commit";
+      # zoxide aliases
+      cd = "z";           # replace cd with z
+      cdi = "zi";         # interactive selection
+      cdr = "z -";        # go to previous directory
+      cdf = "z -f";       # force directory change
+      cdh = "z ~";        # go to home directory
     };
   };
 } 
