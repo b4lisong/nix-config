0e5ed6204df0e167318f0954f064968ea5ae8f26
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-17 11:14:23 -0700

    chore/feat: move fonts into base for all systems

--- modules/base.nix
+++ modules/base.nix
@@ -4,5 +4,15 @@
     pkgs.vim
   ];
 
+  # Define a minimum set of fonts for all systems
+  fonts.packages = with pkgs; [
+    nerd-fonts.sauce-code-pro
+    nerd-fonts.symbols-only
+    font-awesome
+    material-design-icons
+    noto-fonts-emoji
+    dejavu_fonts
+  ];
+
   nix.settings.experimental-features = "nix-command flakes";
 }
