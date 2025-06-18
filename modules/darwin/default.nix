1c198198f2bf39155ae664bd15217701cb4e67d9
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-18 15:28:07 -0700

    feat: move most Darwin settings to Darwin defaults

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,7 +3,7 @@
 System-level configuration common to all Darwin/macOS hosts
 Extends base system configuration in `modules/base.nix`
 */
-{ pkgs, ... }:
+{ pkgs, self, ... }:
 {
   imports = [
     ./homebrew.nix # Import default Homebrew configuration
@@ -13,7 +13,67 @@ Extends base system configuration in `modules/base.nix`
   # This allows using fingerprint authentication when running sudo commands
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  # System state version for compatibility
-  # This should be incremented when making breaking changes to the system configuration
-  system.stateVersion = 6;
+  # Allow unfree packages for all Darwin systems
+  nixpkgs.config.allowUnfree = true;
+
+  system = {
+    # System state version for compatibility
+    # This should be incremented when making breaking changes to the system configuration
+    stateVersion = 6;
+
+    configurationRevision = self.rev or self.dirtyRev or null;
+
+    defaults = {
+      menuExtraClock.Show24Hour = true;
+      dock = {
+        autohide = true;
+        show-recents = false;
+        mru-spaces = false;
+        expose-group-apps = true;
+        showhidden = true;
+        autohide-delay = 0.0;
+        autohide-time-modifier = 0.2;
+        orientation = "bottom";
+        tilesize = 48;
+        magnification = true;
+        largesize = 64;
+        minimize-to-application = true;
+      };
+      finder = {
+        _FXShowPosixPathInTitle = true;
+        AppleShowAllExtensions = true;
+        FXEnableExtensionChangeWarning = false;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        FXDefaultSearchScope = "SCcf";
+        FXPreferredViewStyle = "Nlsv";
+      };
+      trackpad = {
+        Clicking = true;
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = true;
+      };
+      ".GlobalPreferences"."com.apple.mouse.scaling" = 1.0;
+      controlcenter.Sound = true;
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleKeyboardUIMode = 3;
+        ApplePressAndHoldEnabled = true;
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+        AppleShowAllExtensions = true;
+        AppleShowScrollBars = "Automatic";
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+        PMPrintingExpandedStateForPrint = true;
+        PMPrintingExpandedStateForPrint2 = true;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+      };
+    };
+  };
 }
