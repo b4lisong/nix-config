a2778eb80cd3dc2d60a94c3d08e35253b5a4e19b
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-06-26 20:40:14 -0700

    feat: add latest node.js and claude-code

--- home/roles/dev/default.nix
+++ home/roles/dev/default.nix
@@ -1,5 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   home.packages = with pkgs; [
+    ## Stable packages
     neovim
+
+    ## Unstable packages
+    pkgs-unstable.nodePackages_latest.nodejs # latest Node.js
+    pkgs-unstable.claude-code # latest Claude Code (npm)
   ];
 }
