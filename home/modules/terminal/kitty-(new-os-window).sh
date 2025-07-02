4959f5460504a955790cdf23330dd29319424719
b4lisong <5397809+b4lisong@users.noreply.github.com>
2025-07-02 11:57:11 -0700

    feat: add Raycast script to launch kitty in new OS window
    this is jank; TODO: do this in a more idiomatic (Nix) way

---
+++ home/modules/terminal/kitty-(new-os-window).sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title kitty (New OS Window)
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Launch kitty in a new OS window
+
+/Users/balisong/.nix-profile/bin/kitty --single-instance
